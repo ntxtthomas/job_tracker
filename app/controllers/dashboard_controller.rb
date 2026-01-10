c678245e029a92f293d28e8072d86aa9bcf92da5
ntxtthomas <apm.tthomas@gmail.com>
2026-01-09 18:17:18 -0600

    add assessed widget

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,6 +5,7 @@ class DashboardController < ApplicationController
     @total_responses = Opportunity.where(status: "responded").count
     @total_interviews = Opportunity.where(status: "interview").count
     @total_offers = Opportunity.where(status: "offer").count
+    @total_assessed = Opportunity.count
 
     # Tech stack analytics
     opportunities_with_tech = Opportunity.joins(:technologies).distinct
@@ -31,9 +32,22 @@ class DashboardController < ApplicationController
     today = Date.today
     weeks = []
 
-    4.downto(1) do |i|
-      week_start = today - (i * 7).days
-      week_end = week_start + 6.days
+    # TWC (Texas Workforce Commission) weeks: Friday through Thursday
+    # Calculate days back to find the most recent Friday
+    days_back_to_friday = (today.wday - 5) % 7
+    current_week_start = today - days_back_to_friday.days
+    current_week_end = current_week_start + 6.days
+
+    # Show current week + 3 previous weeks
+    4.times do |i|
+      if i == 0
+        week_start = current_week_start
+        week_end = current_week_end
+      else
+        week_end = current_week_start - 1.day
+        week_start = week_end - 6.days
+        current_week_start = week_start
+      end
 
       count = Opportunity.where(application_date: week_start..week_end).count
 
